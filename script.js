8c301d770fdee7e6a22c9d50b7b58d47b7f3c9fc
Knizner Tamás <knitomi90@inf.elte.hu>
2016-03-21 23:16:35 +0100

    beta

---
+++ script.js
@@ -0,0 +1,9 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+window.addEventListener('scroll',onScrollEventListener, true);
+
+function onScrollEventListener(e){
+    $('#navHeader').style.marginTop = window.scrollY + "px";
+}
